f1043826cb461a5e14ca3ed255f7a790ba767945
Márton László Attila <emela@emela.hu>
2021-03-02 10:59:49 +0100

    Working react version

--- backend/server.js
+++ backend/server.js
@@ -3,25 +3,27 @@ const fileUpload = require('express-fileupload');
 const app = express();
 const port = 8000;
 
-app.use('/form', express.static(__dirname + '/../frontend/index.html'));
+// app.use('/form', express.static(__dirname + '/../frontend/index.html'));
 
-app.use('/js', express.static(__dirname + '/../frontend/js'))
+// app.use('/js', express.static(__dirname + '/../frontend/js'))
+app.get("/", function (req, res) {
+  res.send("hello");
+})
 
 app.use(fileUpload());
 app.post('/upload', function (req, res) {
   let sampleFile;
   let uploadPath;
 
-  console.log('req.files >>>', req.files.userfile.name, req.body.username); // eslint-disable-line
-  if (!req.files || Object.keys(req.files).length === 0) {
+  if (!req.files || Object.keys(req.files.userfile).length === 0) {
     return res.status(400).send('No files were uploaded.');
   }
 
+  // itt méág vizsgálni kell, hogy a fájl fel van-e töltve!!!!
+
   // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
   sampleFile = req.files.userfile;
-  uploadPath = __dirname + '/../frontend/upload/' + sampleFile.name;
-
-  //console.log(sampleFile, uploadPath)
+  uploadPath = __dirname + '/../frontend/public/upload/' + sampleFile.name;
 
   // Use the mv() method to place the file somewhere on your server
   sampleFile.mv(uploadPath, function (err) {
